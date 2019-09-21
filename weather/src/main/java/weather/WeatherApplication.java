15da36dd412a468b67ab1bfffbb50e0b73f9d230
PSEH-9 <40661616+PSEH-9@users.noreply.github.com>
2019-09-21 17:30:42 +0530

    CHORE: Commit after some cleanup

--- weather/src/main/java/weather/WeatherApplication.java
+++ weather/src/main/java/weather/WeatherApplication.java
@@ -5,12 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 @SpringBootApplication
 public class WeatherApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         SpringApplication.run(WeatherApplication.class, args);
     }
 }
--- weather/src/main/java/weather/WeatherController.java
+++ weather/src/main/java/weather/WeatherController.java
@@ -1,5 +1,9 @@
 package weather;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -18,29 +22,34 @@ public class WeatherController {
     private static final String MODE = ",us&mode=json&appid=d2929e9483efc82c82c32ee7e02d563e";
 
     @RequestMapping(value ="/weather",produces = "application/json")
-    public String getWeather(@RequestParam(value="city", defaultValue="London") String city) throws CityNotFoundException {
+    public String getWeather(@RequestParam(value="city", defaultValue="London") String city) throws CityNotFoundException, ParseException {
         String url = ENDPOINT + city + MODE;
         RestTemplate restTemplate = new RestTemplate();
         Weather forecast = restTemplate.getForObject(url, Weather.class);
         boolean carryUmbrella = false;
         boolean useSunScreen = false;
-        ObjectNode resp = (ObjectNode) forecast.getList().get(0);
-        ObjectNode temp = (ObjectNode) resp.get("main");
-        String weather =  resp.get("weather").get(0).get("main").toString();
-        Double tempMin  = temp.get("temp_min").asDouble();
-        Double tempMax = temp.get("temp_max").asDouble();
-        Double currTemp = temp.get("temp").asDouble();
-        if(weather.equals("Rain")) carryUmbrella=true;
-        if(currTemp > MAX_TEMPERATURE) useSunScreen = true;
-        return forecast.toString(city,tempMin, tempMax, carryUmbrella, useSunScreen);
+        int noOfRecords = Integer.parseInt(forecast.getCnt());
+        int thisMonth = Calendar.getInstance().get(Calendar.MONTH);
+        for(int i = 0; i< noOfRecords; i++) {
+            ObjectNode response = (ObjectNode) forecast.getList().get(i);
+            Long val  = (Long) response.get("dt").asLong();
+            SimpleDateFormat originalFormat = new SimpleDateFormat("yyyyMMdd");
+            Date date = originalFormat.parse(val.toString());
+            if(date.getMonth() == thisMonth) {
+                ObjectNode temp = (ObjectNode) response.get("main");
+                String weather =  response.get("weather").get(0).get("main").toString();
+                Double tempMin  = temp.get("temp_min").asDouble();
+                Double tempMax = temp.get("temp_max").asDouble();
+                Double currTemp = temp.get("temp").asDouble();
+                if(weather.equals("Rain")) carryUmbrella=true;
+                if(currTemp > MAX_TEMPERATURE) useSunScreen = true;
+                return forecast.toString(city,tempMin, tempMax, carryUmbrella, useSunScreen);
+            }
+        }
+        return "No forecast available!";
         // TEST FORECAST RESPONSE
         //return st.toString("Dallas",200.0,300.23,true,true);
-    }
 
-    /*
-    @RequestMapping(value ="/weather/{city}",produces = "application/json")
-    public String getWeather(@RequestParam(value="city", defaultValue="London") String name) {
-        //return JSON response of the weather for the next three days
-    }*/
+    }
 
 }
